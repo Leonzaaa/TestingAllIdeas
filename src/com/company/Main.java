ce0a1c59a86adb12c5134797ade98172867dad77
Leonzaaa <leon.alex69@gmail.com>
2022-07-03 21:11:06 +0300

    two new message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,8 @@ public class Main {
         staticClass.setCount(2);
 
         System.out.println("print message");
+        System.out.println("print message2");
+        System.out.println("print message3");
         System.out.println(StaticClass.count);
     }
 }
